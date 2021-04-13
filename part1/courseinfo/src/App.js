c406ff0394ec39e99d2d62a5b562a0734637f8eb
tanxh33 <54199648+tanxh33@users.noreply.github.com>
2021-04-13 11:31:04 +0800

    Course info shows for arbitrary number of course

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Header = (props) => <h1>{props.course}</h1>;
+const Header = ({ text }) => <h2>{text}</h2>;
 
 // Takes in props containing all the course information.
 const Content = ({ parts }) => (
@@ -28,37 +28,65 @@ const Total = ({ parts }) => (
 
 const Course = ({ course }) => (
   <div>
-    <Header course={course.name} />
+    <Header text={course.name} />
     <Content parts={course.parts} />
     <Total parts={course.parts} />
   </div>
 );
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises : 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  };
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
 
   return (
-    <Course course={course} />
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>
+        <Course key={course.id} course={course} />
+      )}
+    </div>
   );
 };
 
