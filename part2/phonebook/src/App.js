8b4bf841c2007e0cee5e8955303a765405c285d5
tanxh33 <54199648+tanxh33@users.noreply.github.com>
2021-04-14 16:33:32 +0800

    Separate component refactor

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,65 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AddEntryForm = (props) => {
+  const debug = true;
+  const {
+    newName, newPhone, nameHandler, phoneHandler, submitHandler,
+  } = props;
+  return (
+    <form onSubmit={submitHandler}>
+      <div>
+        <div>
+          name:
+          <br />
+          <input value={newName} onChange={nameHandler} />
+        </div>
+        <div>
+          phone:
+          <br />
+          <input value={newPhone} onChange={phoneHandler} />
+        </div>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+      {debug && <div>{`debug: ${newName} ${newPhone}`}</div>}
+    </form>
+  );
+};
+
+const Persons = ({ persons }) => (
+  <ul>
+    {persons.map(
+      (person) => (
+        <Person
+          key={person.id}
+          id={person.id}
+          name={person.name}
+          phoneNum={person.phoneNum}
+        />
+      ),
+    )}
+  </ul>
+);
+
+const Person = ({ id, name, phoneNum }) => (
+  <li>
+    {id}
+    {' '}
+    {name}
+    {' '}
+    {phoneNum}
+  </li>
+);
+
+const Search = ({ searchTerm, searchHandler }) => (
+  <div>
+    Search:
+    <br />
+    <input value={searchTerm} onChange={searchHandler} />
+  </div>
+);
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -37,6 +98,7 @@ const App = () => {
   const addPerson = (e) => {
     e.preventDefault(); // Prevent HTML form submission
     if (persons.find((person) => person.name === newName)) {
+      // eslint-disable-next-line
       alert(`${newName} is already in the phonebook`);
       return;
     }
@@ -62,46 +124,43 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <div>
-        Search:
-        <br />
-        <input value={searchTerm} onChange={handleSearchInputChange} />
-      </div>
+      <Search searchTerm={searchTerm} searchHandler={handleSearchInputChange} />
       <h2>Add a new entry</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          <div>
-            name:
-            <br />
-            <input value={newName} onChange={handleNameInputChange} />
-          </div>
-          <div>
-            phone:
-            <br />
-            <input value={newPhone} onChange={handlePhoneInputChange} />
-          </div>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-        <div>
-          {`debug: ${newName} ${newPhone}`}
-        </div>
-      </form>
+      <AddEntryForm
+        newName={newName}
+        newPhone={newPhone}
+        nameHandler={handleNameInputChange}
+        phoneHandler={handlePhoneInputChange}
+        submitHandler={addPerson}
+      />
       <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(
-          (person) => (
-            <li key={person.id}>
-              {person.name}
-              {' '}
-              {person.phoneNum}
-            </li>
-          ),
-        )}
-      </ul>
+      <Persons persons={personsToShow} />
     </div>
   );
+};
+
+AddEntryForm.propTypes = {
+  newName: PropTypes.string.isRequired,
+  newPhone: PropTypes.string.isRequired,
+  nameHandler: PropTypes.func.isRequired,
+  phoneHandler: PropTypes.func.isRequired,
+  submitHandler: PropTypes.func.isRequired,
+};
+
+Persons.propTypes = {
+  // eslint-disable-next-line
+  persons: PropTypes.array.isRequired,
+};
+
+Person.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  phoneNum: PropTypes.string.isRequired,
+};
+
+Search.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+  searchHandler: PropTypes.func.isRequired,
 };
 
 export default App;
