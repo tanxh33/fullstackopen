20e5995de277f41edf333c12387bd89a3e40f1bd
tanxh33 <54199648+tanxh33@users.noreply.github.com>
2021-04-21 17:36:05 +0800

    phonebook frontend use relative path

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = async () => {
   const request = axios.get(baseUrl);
