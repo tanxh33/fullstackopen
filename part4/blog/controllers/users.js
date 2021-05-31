e6d9892711e731567eac8cefb349055442b7db7e
tanxh33 <54199648+tanxh33@users.noreply.github.com>
2021-05-31 18:29:58 +0800

    fixed jest leftover async

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -15,7 +15,7 @@ usersRouter.post('/', async (request, response) => {
   const { body } = request;
 
   if (body.password.length < 3) {
-    response.status(400).json({ error: 'User validation failed: password is too short. Min length: 3' });
+    return response.status(400).json({ error: 'User validation failed: password is too short. Min length: 3' });
   }
 
   const saltRounds = 10;
