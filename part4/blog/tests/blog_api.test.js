ed8276902722c5559609554162ddaf40b47847a7
tanxh33 <54199648+tanxh33@users.noreply.github.com>
2021-05-31 15:27:08 +0800

    changed scope of test blocks

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -6,25 +6,25 @@ const Blog = require('../models/blog');
 
 const api = supertest(app);
 
-// Re-initialise the database before *every* test.
-beforeEach(async () => {
-  await Blog.deleteMany({});
+describe('HTTP GET: when there are initially some notes saved', () => {
+  // Re-initialise the database before *every* test.
+  beforeEach(async () => {
+    await Blog.deleteMany({});
 
-  // An array of Mongoose objects
-  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
-  // Array of promises created by .save()
-  const promiseArray = blogObjects.map((blog) => blog.save());
-  // Transform promise array into a single promise (executed in parallel)
-  await Promise.all(promiseArray);
+    // An array of Mongoose objects
+    const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+    // Array of promises created by .save()
+    const promiseArray = blogObjects.map((blog) => blog.save());
+    // Transform promise array into a single promise (executed in parallel)
+    await Promise.all(promiseArray);
 
   // Use for...of to guarantee a specific execution order:
   // for (const blog of helper.initialBlogs) {
   //   const blogObject = new Blog(blog);
   //   await blogObject.save();
   // }
-});
+  });
 
-describe('HTTP GET: when there are initially some notes saved', () => {
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -50,145 +50,145 @@ describe('HTTP GET: when there are initially some notes saved', () => {
       expect(blog.id).toBeDefined();
     });
   });
-});
 
-describe('HTTP GET: viewing a specific blog', () => {
-  test('succeeds with a valid id', async () => {
-    const blogsAtStart = await helper.blogsInDb();
-    const blogToView = blogsAtStart[0];
+  describe('HTTP GET: viewing a specific blog', () => {
+    test('succeeds with a valid id', async () => {
+      const blogsAtStart = await helper.blogsInDb();
+      const blogToView = blogsAtStart[0];
 
-    const resultBlog = await api
-      .get(`/api/blogs/${blogToView.id}`)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
+      const resultBlog = await api
+        .get(`/api/blogs/${blogToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
 
-    const processedBlogToView = JSON.parse(JSON.stringify(blogToView));
-    expect(resultBlog.body).toEqual(processedBlogToView);
+      const processedBlogToView = JSON.parse(JSON.stringify(blogToView));
+      expect(resultBlog.body).toEqual(processedBlogToView);
     // console.log(resultBlog.body, processedBlogToView);
-  });
+    });
 
-  test('fails with status code 404 if blog does not exist', async () => {
-    const validNonexistingId = await helper.nonExistingId();
-    // console.log(validNonexistingId);
+    test('fails with status code 404 if blog does not exist', async () => {
+      const validNonexistingId = await helper.nonExistingId();
+      // console.log(validNonexistingId);
 
-    await api
-      .get(`/api/notes/${validNonexistingId}`)
-      .expect(404);
-  });
+      await api
+        .get(`/api/notes/${validNonexistingId}`)
+        .expect(404);
+    });
 
-  test('fails with status code 400 if id is invalid', async () => {
-    const invalidId = '5a3d5da59070081a82a3445';
+    test('fails with status code 400 if id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3445';
 
-    await api
-      .get(`/api/blogs/${invalidId}`)
-      .expect(400);
-  });
-});
-
-describe('HTTP POST: addition of a new note', () => {
-  test('succeeds with valid data', async () => {
-    const newBlog = {
-      title: 'IKINARI STEAK',
-      author: 'Sakura Ayane',
-      url: 'https://yakuza.fandom.com/wiki/Ikinari_Steak',
-      likes: 10,
-    };
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/);
-
-    const blogsAtEnd = await helper.blogsInDb();
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-
-    const contents = blogsAtEnd.map((r) => r.title);
-    expect(contents).toContain('IKINARI STEAK');
-  });
-
-  test('a new post without likes will default it to 0', async () => {
-    const newBlog = {
-      title: 'Bakuretsu Mahou',
-      author: 'Takahashi Rie',
-      url: 'https://konosuba.fandom.com/wiki/Megumin',
-    };
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/);
-
-    const blogsAtEnd = await helper.blogsInDb();
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-    blogsAtEnd.forEach((blog) => {
-      expect(blog.likes).toBeDefined();
+      await api
+        .get(`/api/blogs/${invalidId}`)
+        .expect(400);
     });
   });
 
-  test('blog without title is not added, with status code 400', async () => {
-    const newBlog = {
-      author: 'Grant Sanderson',
-      url: 'https://www.3blue1brown.com/',
-    };
+  describe('HTTP POST: addition of a new note', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: 'IKINARI STEAK',
+        author: 'Sakura Ayane',
+        url: 'https://yakuza.fandom.com/wiki/Ikinari_Steak',
+        likes: 10,
+      };
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400);
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-    const blogsAtEnd = await helper.blogsInDb();
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+      const blogsAtEnd = await helper.blogsInDb();
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+      const contents = blogsAtEnd.map((r) => r.title);
+      expect(contents).toContain('IKINARI STEAK');
+    });
+
+    test('a new post without likes will default it to 0', async () => {
+      const newBlog = {
+        title: 'Bakuretsu Mahou',
+        author: 'Takahashi Rie',
+        url: 'https://konosuba.fandom.com/wiki/Megumin',
+      };
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+
+      const blogsAtEnd = await helper.blogsInDb();
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+      blogsAtEnd.forEach((blog) => {
+        expect(blog.likes).toBeDefined();
+      });
+    });
+
+    test('blog without title is not added, with status code 400', async () => {
+      const newBlog = {
+        author: 'Grant Sanderson',
+        url: 'https://www.3blue1brown.com/',
+      };
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400);
+
+      const blogsAtEnd = await helper.blogsInDb();
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+    });
+
+    test('blog without url is not added, with status code 400', async () => {
+      const newBlog = {
+        title: '3 Blue 1 Brown',
+        author: 'Grant Sanderson',
+      };
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400);
+
+      const blogsAtEnd = await helper.blogsInDb();
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+    });
   });
 
-  test('blog without url is not added, with status code 400', async () => {
-    const newBlog = {
-      title: '3 Blue 1 Brown',
-      author: 'Grant Sanderson',
-    };
+  describe('HTTP DELETE: deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb();
+      const blogToDelete = blogsAtStart[0];
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400);
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204);
 
-    const blogsAtEnd = await helper.blogsInDb();
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+      const blogsAtEnd = await helper.blogsInDb();
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+      const contents = blogsAtEnd.map((r) => r.title);
+      expect(contents).not.toContain(blogToDelete.title);
+    });
   });
-});
 
-describe('HTTP DELETE: deletion of a blog', () => {
-  test('succeeds with status code 204 if id is valid', async () => {
-    const blogsAtStart = await helper.blogsInDb();
-    const blogToDelete = blogsAtStart[0];
+  describe('HTTP PUT: update a blog', () => {
+    test('succeeds when updating number of likes', async () => {
+      const blogsInDb = await helper.blogsInDb();
+      const blogToUpdate = blogsInDb[0];
+      const blog = { ...blogToUpdate, likes: 343 };
 
-    await api
-      .delete(`/api/blogs/${blogToDelete.id}`)
-      .expect(204);
+      const updatedBlog = await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(blog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
 
-    const blogsAtEnd = await helper.blogsInDb();
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
-
-    const contents = blogsAtEnd.map((r) => r.title);
-    expect(contents).not.toContain(blogToDelete.title);
-  });
-});
-
-describe('HTTP PUT: update a blog', () => {
-  test('succeeds when updating number of likes', async () => {
-    const blogsInDb = await helper.blogsInDb();
-    const blogToUpdate = blogsInDb[0];
-    const blog = { ...blogToUpdate, likes: 343 };
-
-    const updatedBlog = await api
-      .put(`/api/blogs/${blogToUpdate.id}`)
-      .send(blog)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
-
-    expect(updatedBlog.body).toEqual(blog);
-    expect(updatedBlog.body.likes).toBe(343);
+      expect(updatedBlog.body).toEqual(blog);
+      expect(updatedBlog.body.likes).toBe(343);
+    });
   });
 });
 
