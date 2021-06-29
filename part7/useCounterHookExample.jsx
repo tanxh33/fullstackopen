0bc586f58f09a9fc11ad7940b0d494133ede4a1b
tanxh33 <54199648+tanxh33@users.noreply.github.com>
2021-06-29 14:02:02 +0800

    custom hook examples

---
+++ part7/useCounterHookExample.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+// We extract the counter logic into its own custom hook.
+const useCounter = () => {
+  const [value, setValue] = useState(0)
+
+  const increase = () => {
+    setValue(value + 1)
+  }
+
+  const decrease = () => {
+    setValue(value - 1)
+  }
+
+  const zero = () => {
+    setValue(0)
+  }
+
+  return {
+    value, 
+    increase,
+    decrease,
+    zero
+  };
+};
+
+const CounterNoHook = (props) => {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(counter - 1)}>
+        minus
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+    </div>
+  );
+};
+
+const Counter = (props) => {
+  const counter = useCounter()
+
+  return (
+    <div>
+      <div>{counter.value}</div>
+      <button onClick={counter.increase}>
+        plus
+      </button>
+      <button onClick={counter.decrease}>
+        minus
+      </button>
+      <button onClick={counter.zero}>
+        zero
+      </button>
+    </div>
+  );
+};
+
+const Clicker = () => {
+  // Create two counters with their own state but same logic.
+  const left = useCounter()
+  const right = useCounter()
+
+  return (
+    <div>
+      {left.value}
+      <button onClick={left.increase}>
+        left
+      </button>
+      <button onClick={right.increase}>
+        right
+      </button>
+      {right.value}
+    </div>
+  );
+};
---
+++ part7/useFieldHookExmple.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+
+// Input parameter is type of the input field
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+
+const GenericFormNoHook = () => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+  const [height, setHeight] = useState('')
+
+  return (
+    <div>
+      <form>
+        name: 
+        <input
+          type='text'
+          value={name}
+          onChange={(event) => setName(event.target.value)} 
+        /> 
+        <br/> 
+        birthdate:
+        <input
+          type='date'
+          value={born}
+          onChange={(event) => setBorn(event.target.value)}
+        />
+        <br /> 
+        height:
+        <input
+          type='number'
+          value={height}
+          onChange={(event) => setHeight(event.target.value)}
+        />
+      </form>
+      <div>
+        {name} {born} {height} 
+      </div>
+    </div>
+  )
+}
+
+const GenericForm = () => {
+  const name = useField('text')
+  const born = useField('date')
+  const height = useField('number')
+
+  return (
+    <div>
+      <form>
+        <input
+          type={name.type}
+          value={name.value}
+          onChange={name.onChange} 
+        />
+        <input
+          type={born.type}
+          value={born.value}
+          onChange={born.onChange} 
+        />
+        <input
+          type={height.type}
+          value={height.value}
+          onChange={height.onChange} 
+        />
+      </form>
+      <div>
+        {name.value} {born.value} {height.value}
+      </div>
+    </div>
+  )
+}
+
+const GenericFormWithSpread = () => {
+  const name = useField('text')
+  const born = useField('date')
+  const height = useField('number')
+
+  return (
+    <div>
+      <form>
+        name: 
+        <input  {...name} /> 
+        <br/> 
+        birthdate:
+        <input {...born} />
+        <br /> 
+        height:
+        <input {...height} />
+      </form>
+      <div>
+        {name.value} {born.value} {height.value}
+      </div>
+    </div>
+  )
+}
