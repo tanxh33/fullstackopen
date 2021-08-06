bd4515a35ee2146aabb512b22155ea91ea2c429f
tanxh33 <54199648+tanxh33@users.noreply.github.com>
2021-08-06 15:03:06 +0800

    typescript setup exercises

---
+++ part9/ts-setup/calculator.ts
@@ -0,0 +1,27 @@
+// Union type of string literal types
+// `Operation` is a type alias
+type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op: Operation): number => {
+  switch(op) {
+    case 'multiply':
+      return a * b;
+    case 'add':
+      return a + b;
+    case 'divide':
+      if (b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    default:
+      throw new Error('Operation is not multiply, add, or divide!');
+  }
+}
+
+try {
+  calculator(1, 4, 'add');
+  calculator(6, 4, 'multiply');
+  calculator(1, 4, 'divide');
+  calculator(1, 0, 'divide');
+  // calculator(1, 2, 'yolo');
+} catch (e) {
+  console.error(`Something went wrong. Error message: ${e.message}`);
+}
